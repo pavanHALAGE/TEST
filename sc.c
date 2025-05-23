1f4ef105dcc67e14b47ba7cef9202c67db5e3503
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-05-23 11:36:07 +0530

    b2 branch is created

--- sc.c
+++ sc.c
@@ -12,3 +12,30 @@ int main() {
     printf("Enter two operands: ");
     scanf("%lf %lf", &num1, &num2);
 
+
+     // Perform the operation based on the operator
+    switch (operator) {
+        case '+':
+            result = add(num1, num2);
+            printf("%.2f + %.2f = %.2f\n", num1, num2, result);
+            break;
+        case '-':
+            result = subtract(num1, num2);
+            printf("%.2f - %.2f = %.2f\n", num1, num2, result);
+            break;
+        case '*':
+            result = multiply(num1, num2);
+            printf("%.2f * %.2f = %.2f\n", num1, num2, result);
+            break;
+        case '/':
+            if (num2 != 0.0) {
+                result = divide(num1, num2);
+                printf("%.2f / %.2f = %.2f\n", num1, num2, result);
+            } else {
+                printf("Error! Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Error! Invalid operator.\n");
+    }
+
