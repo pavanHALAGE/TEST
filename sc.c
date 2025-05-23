55093d584597f3828035f0b3724871995bab84bd
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-05-23 11:15:33 +0530

    .c file is created

---
+++ sc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2;
+
+    // Prompt the user to enter an operator
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);  // Note the space before %c to consume any leftover whitespace
+
+    // Prompt the user to enter two operands
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &num1, &num2);
+
